0820ac17184b629aefe9fbc1fea558ebb613b07c
Georges Haddad <prismosoft@gmail.com>
2022-04-06 20:22:54 -0400

    no message

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ module.exports = {
   entry: './index.js',
   output: {
     path: './dist',
-    filename: 'vue-sessionstorage.min.js',
+    filename: 'vue-session-storage.min.js',
     library: 'VueSessionStorage',
     libraryTarget: 'umd'
   },
